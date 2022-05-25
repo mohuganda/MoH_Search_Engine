c61177ed6e006a6fce762ae017b02f97919d3575
Henry Nkuke <henry.nkuke@interswitchng.com>
2022-05-25 22:12:01 +0300

    Default string fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\SearchRepository;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,6 +32,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
    
+
+        Schema:defaultStringLength(191);
 
     }
 }
