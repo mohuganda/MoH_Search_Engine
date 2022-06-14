53aafc573578e86e0bd9425de1a151b712e2d83a
Agaba Andrew <agabaandre@gmail.com>
2022-06-14 23:36:26 +0300

    buttons color update

--- resources/views/layouts/partials/admin/topnav.blade.php
+++ resources/views/layouts/partials/admin/topnav.blade.php
@@ -49,7 +49,7 @@
 
 				<li>
 					<a href="{{url('/cms/persons')}}" class="has-arrow">
-						<i class="bx bx-person"></i>
+						<i class="fa fa-address-book-o"></i>
 						{{ __('general.contact') }} {{ trans_choice('general.person',2) }}
 					</a>
 				</li>
