5af8f5429db1e8d7de161a703613f8fae29b1af5
Agaba Andrew <agabaandre@gmail.com>
2022-06-15 01:35:32 +0300

    search icon

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link rel="icon" href="{{ asset('admin/images/favicon-32x32.png') }}" type="image/png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{asset('theme/css/font-awesome.min.css')}}" >
     <link rel="stylesheet" href="{{asset('theme/css/ionicons.min.css')}}">
