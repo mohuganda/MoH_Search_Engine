c22630aa822e8ac523d592ca55c7f71ec0ce9aa4
Agaba Andrew <agabaandre@gmail.com>
2022-06-15 00:52:55 +0300

    index formmating

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -57,8 +57,46 @@
 			<div class="card-body">
 				<div class="d-flex align-items-center">
 					<div>
-						<h5 class="mb-1">Top Products</h5>
-						<p class="mb-0 font-13 text-secondary"><i class='bx bxs-calendar'></i>in last 30 days revenue</p>
+						<h5 class="mb-1">Top Search Keywords</h5>
+						<p class="mb-0 font-13 text-secondary"><i class='bx bxs-calendar'></i>in last 30 days</p>
+					</div>
+					<div class="font-22 ms-auto"><i class='bx bx-dots-horizontal-rounded'></i>
+					</div>
+				</div>
+			</div>
+			<div class="product-list p-3 mb-3">
+				<div class="row border mx-0 mb-3 py-2 radius-10 cursor-pointer">
+					<div class="col-sm-6">
+						<div class="d-flex align-items-center">
+							<div class="product-img">
+								<img src="{{ asset('admin/images/icons/chair.png') }}" alt="" />
+							</div>
+							<div class="ms-2">
+								<h6 class="mb-1">Light Blue Chair</h6>
+								<p class="mb-0">$240.00</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm">
+						<h6 class="mb-1">$2140.00</h6>
+						<p class="mb-0">345 Sales</p>
+					</div>
+					<div class="col-sm">
+						<div id="chart5"></div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="col d-flex">
+		<div class="card radius-10 w-100">
+			<div class="card-body">
+				<div class="d-flex align-items-center">
+					<div>
+						<h5 class="mb-1">Top Searched Dashboards</h5>
+						<p class="mb-0 font-13 text-secondary"><i class='bx bxs-calendar'></i>in last 30 days</p>
 					</div>
 					<div class="font-22 ms-auto"><i class='bx bx-dots-horizontal-rounded'></i>
 					</div>
