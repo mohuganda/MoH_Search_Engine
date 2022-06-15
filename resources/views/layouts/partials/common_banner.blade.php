04fd14e04e8e351bee18b23394ef7a5289993d68
Agaba Andrew <agabaandre@gmail.com>
2022-06-16 01:27:40 +0300

    formatting

--- resources/views/layouts/partials/common_banner.blade.php
+++ resources/views/layouts/partials/common_banner.blade.php
@@ -1,13 +1,12 @@
 <section class="hero-wrap" style="background-image: url({{ asset('images/kla.jpg') }}); background-size: cover; background-position: center; background-repeat: no-repeat;">
     <div class="overlay"></div>
     <div class="container">
-    <div class="row no-gutters slider-text">
-    <div class="col-lg-2 text-center align-items-left" style="float:left !important;">
-                
-    </div>
-</div>
         <div class="row no-gutters slider-text">
-          
+            <div class="col-lg-2 text-center align-items-left" style="float:left !important;">
+                <a class="navbar-brand" style="color:#FFF; font-size:14px;" href="{{url('')}}">Home</a>
+            </div>
+        </div>
+        <div class="row no-gutters slider-text">
             <div class="col-lg-2 text-center align-items-left" style="float:left !important;">
                 <a class="navbar-brand" href="{{url('')}}">
                     <img src="{{asset('theme/images/moh.png')}}" width="100px">
