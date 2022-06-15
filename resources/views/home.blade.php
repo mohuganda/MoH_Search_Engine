c160aa1a3a90f54cb13ea9575298fcd053beadd2
Agaba Andrew <agabaandre@gmail.com>
2022-06-15 23:54:23 +0300

    nav link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row no-gutters slider-text">
             <div class="col-lg-2 text-center align-items-left" style="float:left !important;">
-
+                
                 <a class="navbar-brand" href="{{url('')}}">
                     <img src="{{asset('theme/images/moh.png')}}" width="100px">
                     <p style="color:#FFF; text-align:center; font-weight:200; font-size:small;">Ministry of Health</p>
