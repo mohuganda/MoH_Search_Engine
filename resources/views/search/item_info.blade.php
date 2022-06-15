aed0951b76dfcaeb0e25f6859ba91c23842a4eea
Agaba Andrew <agabaandre@gmail.com>
2022-06-15 22:20:05 +0300

    details

--- resources/views/search/item_info.blade.php
+++ resources/views/search/item_info.blade.php
@@ -16,16 +16,51 @@
 
 		@endphp
 
-        {{ $item->title }}
-        {{ ($item->organization)?$item->organization->organization_name:'' }}
+ 
+                <div class="col-md-12 col-lg-12 d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
+                    <div class="listing-wrap">
 
-        <br>
-        <h3>Person: </h3>
-       <br>
-       @php 
-        print_r(@get_item_contact($item->id));
+                        <div class="text text-center">
+                            <div class="d-flex justify-content-left search-text">
+                                <a href="{{ asset('images/'.$item->image) }}"  >
+                                    <img src="{{ asset('images/'.$item->image) }}" width="200px"  /></a>
+                                <a
+                                    onclick="logAccess({!! $item->id !!})" href="{{ $item->url_link }}" target="_blank">{!! highlight($item->title,$term) !!}</a>
 
-       @endphp
+                            </div>
+                            <div class="d-flex justify-content-left description" style="clear:both; text-align:left; margin-right:6px;">
+
+                                <p>{!! highlight($item->description,$term) !!}</p>
+                                
+                            </div>
+                          
+                            <div class="info-wrap2 align-items-center description">
+
+
+                                <p class="review text-truncate">
+                                    <span class="rev"><small>
+                                    <a href="{{ $item->url_link }}"  target="_blank">{{ $item->url_link }}</a></small>
+                                </span>
+                                </p>
+                                
+
+                                <p class="review"><span class="rev">Theme: <small>{{$item->thematic_area->description}}</small></span> |
+                                <span class="rev">Type: <small>{{ $item->item_type->item_type_name }}</small></span> |
+                               <span class="rev">Access: <small>{{ $item->access_method }}</small></span> | 
+
+                                @if($item->access_method == 'Access Required')
+                                <span class="rev"><small>
+                                    <a href="{{ url('/access',$item->id) }}" target="_blank">{{ __('general.request_access')}}</a></small>
+                                </span> | 
+                                @endif
+                                <span class="rev"><small>
+                                    <a href="{{ url('/iteminfo',$item->id) }}" target="_blank">{{ __('general.moreinfo')}}</a></small>
+                                </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
      
     </div>
 
