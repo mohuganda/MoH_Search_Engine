344390a8a5a619cd02053ba501f4151aef408a8b
Agaba Andrew <agabaandre@gmail.com>
2022-06-24 10:12:42 +0300

    langauge fix

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -19,16 +19,7 @@
   function googleTranslateElementInit() {
   new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false }, 'google_translate_element');
 }
-function translateLanguage(lang) {
-  googleTranslateElementInit();
-  var $frame = $('.goog-te-menu-frame:first');
-  if (!$frame.size()) {
-    alert("Error: Could not find Google translate frame.");
-    return false;
-  }
-  $frame.contents().find('.goog-te-menu2-item span.text:contains(' + lang + ')').get(0).click();
-  return false;
-}
+
 
 </script>
 
