c334f2f936424e7d644a2573eec574c3dfaf0624
Agaba Andrew <agabaandre@gmail.com>
2022-07-07 21:38:10 +0300

    admin debuging

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,97 @@
-<h1>Login</h1>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title> MOH Dashboards Portal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{asset('login/css/style.css')}}">
+
+</head>
+
+<body>
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-12 col-lg-10">
+                    <div class="wrap d-md-flex">
+                        <div class="img justify-content-center" style="background-image: url({{ asset('images/kla.jpg') }}); display: flex; align-items: center; justify-content: center;">
+                            <div class="col-md-6 text-center mb-5">
+                                <a class="navbar-brand" href="{{url('')}}">
+                                    <img src="{{asset('theme/images/moh.png')}}" width="100px">
+                                    <p style="color:#FFF; text-align:center; font-weight:400; font-size:small;">Ministry of Health</p>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="login-wrap p-2 p-md-5">
+                            <div class="d-flex align-items-center; justify-content-center;">
+                                <div class="w-100">
+                                    <h3 class="heading-section">MoH Dashboard Portal</h3>
+                                    <h4 class="mb-4 justify-content-center">Sign In</h4>
+                                </div>
+
+                            </div>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <label class="label" for="name">{{ __('Email Address') }}</label>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="label" for="password">{{ __('Password') }}</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="form-control btn btn-primary rounded submit px-3"> {{ __('Login') }}</button>
+                                </div>
+                                <div class="form-group d-md-flex">
+                                    <div class="w-50 text-left">
+                                        <label class="checkbox-wrap checkbox-primary mb-0"> {{ __('Remember Me') }}
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                    <div class="w-50 text-md-right">
+                                        @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                        @endif
+                                    </div>
+                                </div>
+                            </form>
+                            <!-- <p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="{{asset('login/js/jquery.min.js')}}"></script>
+    <script src="{{asset('login/js/popper.js')}}"></script>
+    <script src="{{asset('login/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('login/js/main.js')}}"></script>
+
+</body>
+
+</html>
