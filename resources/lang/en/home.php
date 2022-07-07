cefa37977b142a7ef83d186f5e113a9445d83b40
Agaba Andrew <agabaandre@gmail.com>
2022-07-07 19:12:51 +0300

    taso facs

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -4,7 +4,7 @@ return [
     "message"=>"Message",
     "Search_Dashboards"=>"Search for Dashboards within the Uganda Health Sector",
     "Search_Results"=>"Your Search Results",
-    'Search for'=>'Search  for',
+    'search_for'=>'Search for',
     'with_in_sector'=>'within the health sector',
     'one_stop_center'=>'A one stop center for finding'
 
