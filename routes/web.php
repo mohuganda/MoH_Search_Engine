36d19818b679be852076bb33c7db02cc6f12f1a6
Agaba Andrew <agabaandre@gmail.com>
2022-07-07 20:27:19 +0300

    web route

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::get('/suggestions',[SearchController::class,'getSuggestions']);
 Route::resource('/submissions',PublicItemController::class);
 
 Route::get('/admin', function(){
+  //
 	return redirect( url('/cms/home'));
 });
 
