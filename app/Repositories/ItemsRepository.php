b4213fb1e86ed32efb04102717155019a1350f36
Agaba Andrew <agabaandre@gmail.com>
2022-07-07 22:37:24 +0300

    routes

--- app/Repositories/ItemsRepository.php
+++ app/Repositories/ItemsRepository.php
@@ -20,40 +20,42 @@ class ItemsRepository
 {
 
 
-	function getAllItems(Request $request){
+	function getAllItems(Request $request)
+	{
 
 		$area = $request->area;
 		$term = $request->term;
 
-		if($term){
+		if ($term) {
 
-		 $query =  Item::where('title', 'like', '%' . $term . '%')
-					->orWhere('description', 'like', '%' . $term . '%')
-					->orWhere('access_method', 'like', '%' . $term . '%')
-					->orWhere('url_link', 'like', '%' . $term . '%')
-					->orWhere('department', 'like', '%' . $term . '%')
-					->orWhere('hosting_organiation', 'like', '%' . $term . '%')
-					->orWhere('title', 'like', '%' . rephrase($term,5) . '%')
-					->orWhere('description', 'like', '%' . rephrase($term) . '%')
-					->orderBy('title','asc');
+			$query =  Item::where('title', 'like', '%' . $term . '%')
+				->orWhere('description', 'like', '%' . $term . '%')
+				->orWhere('access_method', 'like', '%' . $term . '%')
+				->orWhere('url_link', 'like', '%' . $term . '%')
+				->orWhere('department', 'like', '%' . $term . '%')
+				->orWhere('hosting_organiation', 'like', '%' . $term . '%')
+				->orWhere('title', 'like', '%' . rephrase($term, 5) . '%')
+				->orWhere('description', 'like', '%' . rephrase($term) . '%')
+				->orderBy('title', 'asc');
 
-		return $query->paginate(15);
-
-		}else{
+			return $query->paginate(15);
+		} else {
 
 			return Item::paginate(15);
 		}
 	}
 
-	function getItem($id){
+	function getItem($id)
+	{
 		return Item::find($id);
 	}
 
 
-	function saveItem(Request $request,$contactsIsExternal=false,$id=null){
+	function saveItem(Request $request, $contactsIsExternal = false, $id = null)
+	{
 
-		$item = ($id)? Item::find($id): new Item();
-		
+		$item = ($id) ? Item::find($id) : new Item();
+
 		$item->title    = $request->title;
 		$item->url_link = $request->url;
 		$item->hosting_organiation = $request->organization;
@@ -63,103 +65,104 @@ class ItemsRepository
 		$item->item_type_id        = $request->item_type;
 		$item->description         = $request->description;
 		$item->status 			   = 1;
-		$item->published 		   = ($contactsIsExternal)?0: $request->published; 
+		$item->published 		   = ($contactsIsExternal) ? 0 : $request->published;
 		$item->department		   = 1;
 
-		if($request->file('image')){
+		if ($request->file('image')) {
 
-            $file= $request->file('image');
-            $filename= date('YmdHi').$file->getClientOriginalName();
-            $file->move(public_path('images'), $filename);
-            $item->image=$filename;
-        }
+			$file = $request->file('image');
+			$filename = date('YmdHi') . $file->getClientOriginalName();
+			$file->move(public_path('images'), $filename);
+			$item->image = $filename;
+		}
 
 
-        if($request->dev_entity)
-        	$item->dev_entity_id = $request->dev_entity;
+		if ($request->dev_entity)
+			$item->dev_entity_id = $request->dev_entity;
 
-        if($request->approval_authority)
-        	$item->approval_authority_id = $request->approval_authority;
+		if ($request->approval_authority)
+			$item->approval_authority_id = $request->approval_authority;
 
-        if($request->uitool)
-        	$item->ui_tool_id = $request->uitool; 
+		if ($request->uitool)
+			$item->ui_tool_id = $request->uitool;
 
 		//save the item
-		$saved = ($id)?$item->update():$item->save();
+		$saved = ($id) ? $item->update() : $item->save();
 
-		
+
 		//save thematic areas
 
-		for($i=0;$i<count($request->thematic_areas);$i++){
+		for ($i = 0; $i < count($request->thematic_areas); $i++) {
 
 			$thematic_areas  = $request->thematic_areas;
 
-			$area = ItemThematicArea::firstOrNew(array('thematic_area_id' => $thematic_areas[$i],'item_id'=>$item->id));
+			$area = ItemThematicArea::firstOrNew(array('thematic_area_id' => $thematic_areas[$i], 'item_id' => $item->id));
 			$area->save();
 		}
 
 		//save contacts
-		if($request->contact && !$contactsIsExternal){
+		if ($request->contact && !$contactsIsExternal) {
 
-        	$contact = ItemContactPerson::firstOrNew(
-        	array('contact_person_id' => $request->contact,'item_id'=>$item->id ));
-        	$contact->save();
-        }
+			$contact = ItemContactPerson::firstOrNew(
+				array('contact_person_id' => $request->contact, 'item_id' => $item->id)
+			);
+			$contact->save();
+		}
 
 		//contacts for submission
-		if($contactsIsExternal){
+		if ($contactsIsExternal) {
 			//loop through contacts array, create the contact record, associate with item
 
-			for($i=0; $i<count($request->name);$i++):
+			for ($i = 0; $i < count($request->name); $i++) :
 
-			$names  = $request->name;
-			$emails = $request->email;
-			$phones = $request->phone;
-			$titles = $request->person_title;
-			
-			$person = ContactPerson::firstOrNew(array('phone_number' => $phones[$i]));
+				$names  = $request->name;
+				$emails = $request->email;
+				$phones = $request->phone;
+				$titles = $request->person_title;
 
-			$person->name = $names[$i];
-			$person->phone_number = $phones[$i];
-			$person->email = $emails[$i];
-			$person->title = $titles[$i];
-			$person->organization_id = $item->hosting_organiation;
-			$person->save();
+				$person = ContactPerson::firstOrNew(array('phone_number' => $phones[$i]));
 
-        	$contact = ItemContactPerson::firstOrNew(array('contact_person_id' => $person->id,'item_id'=>$item->id ));
-        	$contact->save();
+				$person->name = $names[$i];
+				$person->phone_number = $phones[$i];
+				$person->email = $emails[$i];
+				$person->title = $titles[$i];
+				$person->organization_id = $item->hosting_organiation;
+				$person->save();
+
+				$contact = ItemContactPerson::firstOrNew(array('contact_person_id' => $person->id, 'item_id' => $item->id));
+				$contact->save();
 
 			endfor;
-        }
-        
-        return $saved;
-
-	}
-
-	function getAllThematicAreas($request=null){
-
-		
-		if(@$request->term){
-
-		$term = $request->term;
-
-		 $query =  ThematicArea::where('description', 'like', '%' . $term . '%')
-					->orderBy('display_index','asc');
-
-		 return $query->paginate(15);
-
-		}else{
-
-			return ThematicArea::orderBy('display_index','asc')->paginate(20);
 		}
-		
+
+		return $saved;
 	}
 
-	function getAllItemTypes(){
+	function getAllThematicAreas($request = null)
+	{
+
+
+		if (@$request->term) {
+
+			$term = $request->term;
+
+			$query =  ThematicArea::where('description', 'like', '%' . $term . '%')
+				->orderBy('display_index', 'asc');
+
+			return $query->paginate(15);
+		} else {
+
+			return ThematicArea::orderBy('display_index', 'asc')->paginate(20);
+		}
+	}
+
+	function getAllItemTypes()
+	{
 		return ItemType::paginate(1000);
 	}
 
-	function saveType(Request $request){
+	function saveType(Request $request)
+	{
 
 		$type = new ItemType();
 		$type->item_type_name = $request->type_name;
@@ -167,67 +170,78 @@ class ItemsRepository
 		return $type->save();
 	}
 
-	function saveThematicArea(Request $request){
+	function saveThematicArea(Request $request)
+	{
 
-		$area = ($request->id)? ThematicArea::find($request->id): new ThematicArea();
+		$area = ($request->id) ? ThematicArea::find($request->id) : new ThematicArea();
 		$area->description = $request->description;
 		$area->icon = $request->icon;
 		$area->display_index = $request->index;
 		return $area->save();
 	}
 
-	function deleteThematicArea($id){
+	function deleteThematicArea($id)
+	{
 		return ThematicArea::findOrFail($id)->delete();
 	}
-//front CMS submission
-function savePublicItem(Request $request,$id=null){
+	//front CMS submission
+	function savePublicItem(Request $request, $id = null)
+	{
 
-	$item = ($id)? Item::find($id): new Item();
-	
-	$item->title    = $request->title;
-	$item->url_link = $request->url;
-	$item->hosting_organiation = $request->organization;
-	$item->access_method 	   = $request->access_method;
-	$item->thematic_area_id    = $request->thematic_area;
-	$item->db_engine 		   = $request->db_engine;
-	$item->item_type_id        = $request->item_type;
-	$item->description         = $request->description;
-	$item->status 			   = 1;
-	$item->department		   = 1;
+		$item = ($id) ? Item::find($id) : new Item();
 
-	if($request->file('image')){
+		$item->title    = $request->title;
+		$item->url_link = $request->url;
+		$item->hosting_organiation = $request->organization;
+		$item->access_method 	   = $request->access_method;
+		$item->thematic_area_id    = 0;
+		//$item->thematic_area_id    = $request->thematic_area;
+		$item->db_engine 		   = $request->db_engine;
+		$item->item_type_id        = $request->item_type;
+		$item->description         = $request->description;
+		$item->status 			   = 1;
+		$item->department		   = 1;
 
-		$file= $request->file('image');
-		$filename= date('YmdHi').$file->getClientOriginalName();
-		$file->move(public_path('images'), $filename);
-		$item->image=$filename;
+		if ($request->file('image')) {
+
+			$file = $request->file('image');
+			$filename = date('YmdHi') . $file->getClientOriginalName();
+			$file->move(public_path('images'), $filename);
+			$item->image = $filename;
+		}
+
+		//save thematic areas
+
+		for ($i = 0; $i < count($request->thematic_areas); $i++) {
+
+			$thematic_areas  = $request->thematic_areas;
+
+			$area = ItemThematicArea::firstOrNew(array('thematic_area_id' => $thematic_areas[$i], 'item_id' => $item->id));
+			$area->save();
+		}
+
+
+
+		if ($request->dev_entity)
+			$item->dev_entity_id = $request->dev_entity;
+
+		if ($request->approval_authority)
+			$item->approval_authority_id = $request->approval_authority;
+
+		if ($request->uitool)
+			$item->ui_tool_id = $request->uitool;
+
+
+		$saved = ($id) ? $item->update() : $item->save();
+
+		if ($request->contact) {
+
+			$contact = ItemContactPerson::firstOrNew(
+				array('contact_person_id' => $request->contact, 'item_id' => $item->id)
+			);
+			$contact->save();
+		}
+
+		return $saved;
 	}
-
-
-	if($request->dev_entity)
-		$item->dev_entity_id = $request->dev_entity;
-
-	if($request->approval_authority)
-		$item->approval_authority_id = $request->approval_authority;
-
-	if($request->uitool)
-		$item->ui_tool_id = $request->uitool; 
-
-
-	$saved = ($id)?$item->update():$item->save();
-
-	if($request->contact){
-
-		$contact = ItemContactPerson::firstOrNew(
-		array('contact_person_id' => $request->contact,'item_id'=>$item->id ));
-		$contact->save();
-	}
-	
-	return $saved;
-
 }
-
-}
-
-
-?>
--- resources/views/search/submit_item.blade.php
+++ resources/views/search/submit_item.blade.php
@@ -5,212 +5,212 @@
 
 
 <form method="post" action="{{url('submissions')}}">
-<div class="card radius-10">
-  <div class="card-body">
-    <div class="col-lg-12" style="text-align:center;">
-     <h3>Submit your Dashboard for Publishing</h3>
-  </div>
-    <div class="row ml-2 mr-2" style="margin-bottom:100px; color:#000;">
-    <div class="row">
+  <div class="card radius-10">
+    <div class="card-body">
+      <div class="col-lg-12" style="text-align:center;">
+        <h3>Submit your Dashboard for Publishing</h3>
+      </div>
+      <div class="row ml-2 mr-2" style="margin-bottom:100px; color:#000;">
+        <div class="row">
 
-  
+
           @if(!empty($alert))
 
           <!-- Success alert -->
-        <div class="alert alert-info bg-white alert-styled-left alert-arrow-left alert-dismissible">
-            <span class="alert_msg">{!! $alert!!}</span>
-             <!-- <a  class="close" data-bs-dismiss="alert"><span>&times;</span></a> -->
+          <div class="alert alert-info bg-white alert-styled-left alert-arrow-left alert-dismissible">
+            <span class="alert_msg">{{$alert}}</span>
+            <!-- <a  class="close" data-bs-dismiss="alert"><span>&times;</span></a> -->
           </div>
           <!-- /Success alert -->
           @endif
-    
 
-  @csrf
-  <div class="col-lg-6">
-      <div class="form-group">
-        <label>{{__('cms.title')}}</label>
-        <input type="text" value="{{@$item->title}}" name="title" class="form-control" placeholder="{{__('cms.title')}}" required>
-      </div>
-      <div class="form-group">
-        <label>{{ __('cms.hosting') }} {{__('cms.organization')}}</label>
-        <select name="organization" class="form-control" required>
-          <option  value="">Choose</option>
-          @foreach($organizations as $row)
-            <option {{ (@$item->hosting_organiation==$row->id)?'selected':'' }} value="{{$row->id}}">{{$row->organization_name}}</option>
-          @endforeach
 
-        </select>
-      </div>
-       <div class="form-group">
-        <label>{{__('cms.item_type')}}</label>
-        <select name="item_type" class="form-control" required>
-          <option  value="">Choose</option>
-           @foreach($types as $row)
-            <option {{ (@$item->item_type_id==$row->id)?'selected':'' }} value="{{$row->id}}">{{$row->item_type_name}}</option>
-          @endforeach
-        </select>
-      </div>
+          @csrf
+          <div class="col-lg-6">
+            <div class="form-group">
+              <label>{{__('cms.title')}}</label>
+              <input type="text" value="{{@$item->title}}" name="title" class="form-control" placeholder="{{__('cms.title')}}" required>
+            </div>
+            <div class="form-group">
+              <label>{{ __('cms.hosting') }} {{__('cms.organization')}}</label>
+              <select name="organization" class="form-control" required>
+                <option value="">Choose</option>
+                @foreach($organizations as $row)
+                <option {{ (@$item->hosting_organiation==$row->id)?'selected':'' }} value="{{$row->id}}">{{$row->organization_name}}</option>
+                @endforeach
 
-      <div class="form-group">
-        <label>{{ trans_choice('cms.thematic_area',1)}}</label>
-        <select name="thematic_areas[]" class="form-control" required multiple>
-          <option  value="">Choose</option>
-          @foreach($areas as $row)
-            <option   value="{{$row->id}}">{{$row->description}}</option>
-          @endforeach
-        </select>
-      </div>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>{{__('cms.item_type')}}</label>
+              <select name="item_type" class="form-control" required>
+                <option value="">Choose</option>
+                @foreach($types as $row)
+                <option {{ (@$item->item_type_id==$row->id)?'selected':'' }} value="{{$row->id}}">{{$row->item_type_name}}</option>
+                @endforeach
+              </select>
+            </div>
 
-        <div class="form-group">
-        <label>{{__('cms.approval')}} {{__('cms.authority')}}</label>
-        <select name="approval_authority" class="form-control">
-          <option  value="">Choose</option>
-           @foreach($authorities as $auth)
-            <option {{ (@$auth->id==@$item->approval_authority_id)?'selected':'' }}  value="{{$auth->id}}">{{$auth->authority_name}}</option>
-          @endforeach
-        </select>
-      </div>
+            <div class="form-group">
+              <label>{{ trans_choice('cms.thematic_area',1)}}</label>
+              <select name="thematic_areas[]" class="form-control" required multiple>
+                <option value="">Choose</option>
+                @foreach($areas as $row)
+                <option value="{{$row->id}}">{{$row->description}}</option>
+                @endforeach
+              </select>
+            </div>
 
-        <div class="form-group">
-        <label>{{__('cms.ui')}} {{ trans_choice('cms.tool',1)}} </label>
-        <select name="uitool" class="form-control">
-          <option  value="">Choose</option>
-           @foreach($uitools as $tool)
-            <option {{ (@$tool->id==@$item->ui_tool_id)?'selected':'' }} value="{{$tool->id}}">{{$tool->tool_name}}</option>
-          @endforeach
-        </select>
-      </div>
+            <div class="form-group">
+              <label>{{__('cms.approval')}} {{__('cms.authority')}}</label>
+              <select name="approval_authority" class="form-control">
+                <option value="">Choose</option>
+                @foreach($authorities as $auth)
+                <option {{ (@$auth->id==@$item->approval_authority_id)?'selected':'' }} value="{{$auth->id}}">{{$auth->authority_name}}</option>
+                @endforeach
+              </select>
+            </div>
 
-      
-    <div class="form-group">
-        <label>{{__('cms.description')}}</label>
-        <textarea rows="4" name="description" class="form-control" placeholder="{{__('cms.description')}}" >{{@$item->description}}</textarea> 
-      </div>
-      
-    </div>
-    <div class="col-lg-6">
+            <div class="form-group">
+              <label>{{__('cms.ui')}} {{ trans_choice('cms.tool',1)}} </label>
+              <select name="uitool" class="form-control">
+                <option value="">Choose</option>
+                @foreach($uitools as $tool)
+                <option {{ (@$tool->id==@$item->ui_tool_id)?'selected':'' }} value="{{$tool->id}}">{{$tool->tool_name}}</option>
+                @endforeach
+              </select>
+            </div>
 
-      <div class="form-group">
-        <label>{{__('cms.access_method')}}</label>
-        <select name="access_method" class="form-control" required>
-          <option value="">Choose</option>
-          <option {{ (@$item->access_method=='Restricted')?'selected':'' }} value="Restricted">Restricted</option>
-          <option {{ (@$item->access_method=='Open')?'selected':'' }}  value="Open">Open</option> 
-        </select>
-      </div>
-      
-      <div class="form-group">
-        <label>{{__('cms.link')}}</label>
-        <input type="text" name="url" value="{{@$item->url_link}}"  class="form-control"  placeholder="{{__('cms.link')}}" required>
-      </div>
-      <div class="form-group">
-        <label>{{__('cms.db_engine')}}</label>
-        <input type="text" name="db_engine" value="{{@$item->db_engine}}"  class="form-control"  placeholder="{{__('cms.db_engine')}}">
-      </div>
 
-      
-      <div class="form-group">
-        <label>{{__('cms.dev')}} {{ trans_choice('cms.entity',1)}} </label>
-        <select name="dev_entity" class="form-control" >
-          <option  value="">Choose</option>
-           @foreach($entities as $entity)
-            <option {{ (@$entity->id==@$item->dev_entity_id)?'selected':'' }}  value="{{$entity->id}}">{{$entity->entity_name}}</option>
-          @endforeach
-        </select>
-      </div>
+            <div class="form-group">
+              <label>{{__('cms.description')}}</label>
+              <textarea rows="4" name="description" class="form-control" placeholder="{{__('cms.description')}}">{{@$item->description}}</textarea>
+            </div>
 
-       <div class="form-group">
-        <label style="font-weight:bold;">{{__('general.contact')}} {{ trans_choice('general.person',1)}}</label>
-        <div class="form-group">
-          
-        <div id="contact" style="max-height: 600px; overflow:auto;">
-
-          <input type="button" value="Add More Contact Persons" class="btn btn-primary  mb-2" onclick="addContact()">
-         
-          <select class="form-control mb-2" name="person_title">
-            <option>Mr.</option>
-            <option>Mrs.</option>
-            <option>Dr.</option>
-            <option>Pr.</option>
-            <option>Ms.</option>
-            <option>Hon.</option>
-          </select>
-          <input type="text" name="name[]" class="form-control mb-2" placeholder="Name" class="form-group" required/>
-          <input type="text" class="form-control mb-2" placeholder="Phone Number" name="phone[]" class="form-group mb-2" required/>
-          <input type="text"  class="form-control" name="email[]" placeholder="Email" class="form-group mb-2" required/></div>
-        
           </div>
-        
+          <div class="col-lg-6">
+
+            <div class="form-group">
+              <label>{{__('cms.access_method')}}</label>
+              <select name="access_method" class="form-control" required>
+                <option value="">Choose</option>
+                <option {{ (@$item->access_method=='Restricted')?'selected':'' }} value="Restricted">Restricted</option>
+                <option {{ (@$item->access_method=='Open')?'selected':'' }} value="Open">Open</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label>{{__('cms.link')}}</label>
+              <input type="text" name="url" value="{{@$item->url_link}}" class="form-control" placeholder="{{__('cms.link')}}" required>
+            </div>
+            <div class="form-group">
+              <label>{{__('cms.db_engine')}}</label>
+              <input type="text" name="db_engine" value="{{@$item->db_engine}}" class="form-control" placeholder="{{__('cms.db_engine')}}">
+            </div>
+
+
+            <div class="form-group">
+              <label>{{__('cms.dev')}} {{ trans_choice('cms.entity',1)}} </label>
+              <select name="dev_entity" class="form-control">
+                <option value="">Choose</option>
+                @foreach($entities as $entity)
+                <option {{ (@$entity->id==@$item->dev_entity_id)?'selected':'' }} value="{{$entity->id}}">{{$entity->entity_name}}</option>
+                @endforeach
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label style="font-weight:bold;">{{__('general.contact')}} {{ trans_choice('general.person',1)}}</label>
+              <div class="form-group">
+
+                <div id="contact" style="max-height: 600px; overflow:auto;">
+
+                  <input type="button" value="Add More Contact Persons" class="btn btn-primary  mb-2" onclick="addContact()">
+
+                  <select class="form-control mb-2" name="person_title">
+                    <option>Mr.</option>
+                    <option>Mrs.</option>
+                    <option>Dr.</option>
+                    <option>Pr.</option>
+                    <option>Ms.</option>
+                    <option>Hon.</option>
+                  </select>
+                  <input type="text" name="name[]" class="form-control mb-2" placeholder="Name" class="form-group" required />
+                  <input type="text" class="form-control mb-2" placeholder="Phone Number" name="phone[]" class="form-group mb-2" required />
+                  <input type="text" class="form-control" name="email[]" placeholder="Email" class="form-group mb-2" required />
+                </div>
+
+              </div>
+
+            </div>
+
+
+          </div>
+
+
+
+          <div class="col-lg-6"></div>
+          <div class="col-lg-6">
+            <div class="form-group btn-wrapper">
+              <br>
+              <input type="submit" class="btn btn-primary d-block w-100 d-flex align-items-center justify-content-center p-2" value="Submit">
+            </div>
+          </div>
+        </div>
+
+
+
+
+
       </div>
-
-
-  </div>
-  
-
-
-  <div class="col-lg-6"></div>
-  <div class="col-lg-6">
-  <div class="form-group btn-wrapper">
-        <br>
-         <input type="submit" class="btn btn-primary d-block w-100 d-flex align-items-center justify-content-center p-2" value="Submit">
-      </div>
-  </div>
-</div>
-
-
-
-
-
-  </div>
-</div>
+    </div>
 </form>
 
 
 
-      <script>
-        function logAccess(system_id) {
-          // event.preventDefault();
-          $.ajax({
-            type: 'GET',
-            url: `<?php echo url('/log_access'); ?>/${system_id}`,
-            success: function(data) {
-              console.log(data);
-            }
-          });
-        }
+<script>
+  function logAccess(system_id) {
+    // event.preventDefault();
+    $.ajax({
+      type: 'GET',
+      url: `<?php echo url('/log_access'); ?>/${system_id}`,
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
 
-        function addContact(){
+  function addContact() {
 
-         let  contact_row = '<div class="form-group person"><p>Contact Person</p>';
-         contact_row += '<select class="form-control mb-2" name="person_title">';
-         contact_row +='<option>Mr.</option>';
-         contact_row +='<option>Mrs.</option>';
-         contact_row +='<option>Dr.</option>';
-         contact_row +='<option>Pr.</option>';
-         contact_row +='<option>Ms.</option>';
-         contact_row +='<option>Hon.</option></select>';
-         contact_row +='<input type="text" name="name[]" class="form-control mb-2" placeholder="Name" class="form-group" required>';
-         contact_row +='<input type="text" class="form-control mb-2" placeholder="Phone Number" name="phone[]" class="form-group mb-2" required/>';
-         contact_row +='<input type="text"  class="form-control" name="email[]" placeholder="Email" class="form-group mb-2" required/>';
-         contact_row +='<input type="button" value="Remove Contact Person" class="btn btn-primary   btn-sm  mb-2"  onclick="removeContact($(this))" ></div>'
+    let contact_row = '<div class="form-group person"><p>Contact Person</p>';
+    contact_row += '<select class="form-control mb-2" name="person_title">';
+    contact_row += '<option>Mr.</option>';
+    contact_row += '<option>Mrs.</option>';
+    contact_row += '<option>Dr.</option>';
+    contact_row += '<option>Pr.</option>';
+    contact_row += '<option>Ms.</option>';
+    contact_row += '<option>Hon.</option></select>';
+    contact_row += '<input type="text" name="name[]" class="form-control mb-2" placeholder="Name" class="form-group" required>';
+    contact_row += '<input type="text" class="form-control mb-2" placeholder="Phone Number" name="phone[]" class="form-group mb-2" required/>';
+    contact_row += '<input type="text"  class="form-control" name="email[]" placeholder="Email" class="form-group mb-2" required/>';
+    contact_row += '<input type="button" value="Remove Contact Person" class="btn btn-primary   btn-sm  mb-2"  onclick="removeContact($(this))" ></div>'
 
-         $("#contact").append(contact_row);
-        }
+    $("#contact").append(contact_row);
+  }
 
-        function removeContact(elem){
-          //$("#contact").find("div:last").remove();
-           elem.closest('.person').remove();
-        }
+  function removeContact(elem) {
+    //$("#contact").find("div:last").remove();
+    elem.closest('.person').remove();
+  }
 
-        $('.removeBtn').on('click', function(){
-          
-          console.log( $(this).closest('.person'));
+  $('.removeBtn').on('click', function() {
 
-          $(this).closest('.person').remove();
+    console.log($(this).closest('.person'));
 
-        });
-        
-      </script>
-    
+    $(this).closest('.person').remove();
+
+  });
+</script>
+
 
 @endsection
