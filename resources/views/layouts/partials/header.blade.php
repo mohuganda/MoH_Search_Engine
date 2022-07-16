1e20455a10c6a7f00d2b4acf3680dc4e9b255cb5
Agaba Andrew <agabaandre@gmail.com>
2022-07-17 00:59:26 +0300

    multiple select

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -7,13 +7,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="preconnect" href="https://fonts.gstatic.com/">
   <link rel="icon" href="{{ asset('admin/images/favicon-32x32.png') }}" type="image/png" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{asset('theme/css/font-awesome.min.css')}}">
   <link rel="stylesheet" href="{{asset('theme/css/ionicons.min.css')}}">
   <link rel="stylesheet" href="{{asset('theme/css/A.animate.css%2bflaticon.css%2btiny-slider.css%2bglightbox.min.css%2baos.css%2bstyle.css%2cMcc.UgmKpvvBTJ.css.pagespeed.cf.xhAIVbA-KH.css')}}" />
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="{{asset('theme/css/filter_multi_select.css')}}" />
+  <style>
+    select>.viewbar>.selected-items>.item {
+      margin: 0.125rem 0.25rem 0.125rem 0;
+      padding: 0px 0px 0px 0.5em;
+      display: inline-flex;
+      height: 1.875em;
+      color: #202;
+      background-color: var(--fms-badge-color);
+      border-radius: 1.1em;
+      align-items: center;
+      vertical-align: baseline;
+    }
+  </style>
 
 
 </head>
